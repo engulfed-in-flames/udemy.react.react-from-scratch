46bdfeefb3cfa027b3281da12b9eeb1fbab26c4c
engulfedinflames <recona97@gmail.com>
2024-05-06 16:29:24 +0900

    Adjust tab width to 4

--- projects/03_usepopcorn/src/index.js
+++ projects/03_usepopcorn/src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./StarRating";
+// import "./index.css";
+// import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <App /> */}
+        <StarRating
+            maxRating={5}
+            messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]}
+            defaultRating={3}
+        />
+    </React.StrictMode>
 );
